fa31ac55816fa2f91b1cb2ac2f658ea2ef52e635
canadmin <icanyard@gmail.com>
2019-09-05 16:28:42 +0300

    register capability and some security configuration

--- src/main/java/com/boykot/Boykot/model/User.java
+++ src/main/java/com/boykot/Boykot/model/User.java
@@ -21,6 +21,7 @@ public class User {
     private String fullName;
     private String email;
     private String password;
+    private String ROLE;
 
 
 }
--- src/main/java/com/boykot/Boykot/security/AuthSecurity.java
+++ src/main/java/com/boykot/Boykot/security/AuthSecurity.java
@@ -12,7 +12,7 @@ public class AuthSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/foo").hasRole("ADMIN")
+            http.authorizeRequests().antMatchers("/newBoycott").hasRole("ADMIN")
                 .anyRequest().permitAll();
     }
 }
