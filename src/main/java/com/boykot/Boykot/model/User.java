e1c11c01a22aae46bb7b07e6b092c22075b3c2cd
canadmin <icanyard@gmail.com>
2019-09-05 15:49:13 +0300

    create rest api capability & lombok

--- src/main/java/com/boykot/Boykot/model/User.java
+++ src/main/java/com/boykot/Boykot/model/User.java
@@ -1,11 +1,18 @@
 package com.boykot.Boykot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -15,35 +22,5 @@ public class User {
     private String email;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
